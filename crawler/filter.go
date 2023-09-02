426d37ba5194f2eeffa8c91ff5a884ec9c9bc94f
pingc0y <ppcpingc@gmail.com>
2023-09-02 13:15:50 +0800

    2023/9/2更新

--- crawler/filter.go
+++ crawler/filter.go
@@ -18,7 +18,6 @@ func jsFilter(str [][]string) [][]string {
 		str[i][0] = strings.Replace(str[i][0], "\\/", "/", -1)
 		str[i][0] = strings.Replace(str[i][0], "%3A", ":", -1)
 		str[i][0] = strings.Replace(str[i][0], "%2F", "/", -1)
-		str[i][0] = strings.Replace(str[i][0], "./", "/", -1)
 		//去除不是.js的链接
 		if !strings.HasSuffix(str[i][0], ".js") && !strings.Contains(str[i][0], ".js?") {
 			str[i][0] = ""
@@ -51,7 +50,6 @@ func urlFilter(str [][]string) [][]string {
 		str[i][0] = strings.Replace(str[i][0], "\\/", "/", -1)
 		str[i][0] = strings.Replace(str[i][0], "%3A", ":", -1)
 		str[i][0] = strings.Replace(str[i][0], "%2F", "/", -1)
-		str[i][0] = strings.Replace(str[i][0], "./", "/", -1)
 		//去除不存在字符串和数字的url,判断为错误数据
 		match, _ := regexp.MatchString("[a-zA-Z]+|[0-9]+", str[i][0])
 		if !match {
